b00eb01c3c687812188a98c579953eb1548a1219
Skrungus <torres23124@students.apsb.org>
2022-09-12 15:03:21 +0000

    add platforms and collectables to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -36,6 +36,8 @@
      */
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
+    createPlatform(650,600,0.5,1);
+
     // example:
     createPlatform(400, 250);
     createPlatform(170, 460, 0.5);
